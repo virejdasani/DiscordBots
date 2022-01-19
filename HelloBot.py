cf00cf4863db8ca75e1ac6fb32aba0f545b64841
Virej Dasani <67495678+virejdasani@users.noreply.github.com>
2022-01-19 14:41:30 +0530

    Subscribe to Virej!

--- HelloBot.py
+++ HelloBot.py
@@ -13,7 +13,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello there!')
 
 client.run(TOKEN)
